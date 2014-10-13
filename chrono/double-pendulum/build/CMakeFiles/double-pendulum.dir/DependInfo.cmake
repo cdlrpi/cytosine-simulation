607e92ccd45e110968012179494a801041251edb
qinglongk <qinglongk@gmail.com>
2014-10-13 15:32:09 -0400

    update on chrono example

--- chrono/double-pendulum/build/CMakeFiles/double-pendulum.dir/DependInfo.cmake
+++ chrono/double-pendulum/build/CMakeFiles/double-pendulum.dir/DependInfo.cmake
@@ -1,31 +1,29 @@
 # The set of languages for which implicit dependencies are needed:
-set(CMAKE_DEPENDS_LANGUAGES
+SET(CMAKE_DEPENDS_LANGUAGES
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/K/Research/code/cytosine-simulation/chrono/double-pendulum/src/demo_buildsystem.cpp" "/Users/K/Research/code/cytosine-simulation/chrono/double-pendulum/build/CMakeFiles/double-pendulum.dir/demo_buildsystem.cpp.o"
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kongq2/Research/code/cytosine-simulation/chrono/double-pendulum/src/demo_buildsystem.cpp" "/home/kongq2/Research/code/cytosine-simulation/chrono/double-pendulum/build/CMakeFiles/double-pendulum.dir/demo_buildsystem.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "Clang")
+SET(CMAKE_CXX_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
-set(CMAKE_TARGET_DEFINITIONS
+SET(CMAKE_TARGET_DEFINITIONS
   "BP_USE_FIXEDPOINT_INT_32"
   )
 
 # Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+SET(CMAKE_TARGET_LINKED_INFO_FILES
   )
 
 # The include file search paths:
-set(CMAKE_C_TARGET_INCLUDE_PATH
-  "/System/Library/Frameworks"
-  "/Users/K/Software/chrono-source/src"
-  "/Users/K/Software/chrono-source/src/collision/bullet"
-  "/Users/K/Software/chrono-source/src/collision/gimpact"
-  "/Users/K/Software/chrono-source/src/collision/convexdecomposition/HACD"
-  "/usr/local/include/irrlicht"
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/kongq2/Software/chrono-source/src"
+  "/home/kongq2/Software/chrono-source/src/collision/bullet"
+  "/home/kongq2/Software/chrono-source/src/collision/gimpact"
+  "/home/kongq2/Software/chrono-source/src/collision/convexdecomposition/HACD"
   )
-set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
--- chrono/double-pendulum/build/CMakeFiles/double-pendulum.dir/cmake_clean.cmake
+++ chrono/double-pendulum/build/CMakeFiles/double-pendulum.dir/cmake_clean.cmake
@@ -1,10 +1,10 @@
-file(REMOVE_RECURSE
+FILE(REMOVE_RECURSE
   "CMakeFiles/double-pendulum.dir/demo_buildsystem.cpp.o"
   "double-pendulum.pdb"
   "double-pendulum"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/double-pendulum.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/double-pendulum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
